bbce98294eb0ec3ac06c977fd63909d26ff62e91
linda <62736818+linda-02@users.noreply.github.com>
2025-08-24 17:47:17 +0800

    Update Opwrt-p1_feeds.sh

--- opwrt-23.05/Opwrt-p1_feeds.sh
+++ opwrt-23.05/Opwrt-p1_feeds.sh
@@ -7,9 +7,3 @@ src-git luci https://github.com/coolsnowwolf/luci
 src-git routing https://git.openwrt.org/feed/routing.git
 src-git telephony https://git.openwrt.org/feed/telephony.git
 
-
-# Add Theme package
-rm -rf package/luci-theme-argon
-rm -rf package/luci-app-argon-config
-sudo git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
-sudo git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
